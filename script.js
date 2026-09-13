362d915f13b04d6c90db629b6562e85b2e1cc84e
YashGunjal <238607689+YashGunjal9@users.noreply.github.com>
2026-09-13 19:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Mouse event
+
+const clickButtonEl = document.getElementById("clickButtonn")
+
+function clickHandler() {
+  console.log("Click event is called");
+}
+
+function dblclickHandler() {
+  console.log("dblClick event is called");
+}
+
+function onMouseOverHandler() {
+  console.log("onmouseoverHandler event is called");
+}
+
+function onMouseOutHandler() {
+  console.log("onMouseOutHandler event is called");
+}
+
